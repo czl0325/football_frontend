10d5b228b37845eecd96f8435bcb6abb333be581
陈昭良 <295183917@qq.com>
2023-11-15 10:42:16 +0800

    新建工程

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import 'vant/es/toast/style'
+import 'vant/es/dialog/style'
+import 'vant/es/notify/style'
+import 'vant/es/image-preview/style'
+
+
+createApp(App).mount('#app')
