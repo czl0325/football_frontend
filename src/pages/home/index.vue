182b84464cc96edadd5a7a2dff61c5e501958389
czl <295183917@qq.com>
2023-12-11 21:12:58 +0800

    修改

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="app-container">
     <van-nav-bar title="所有比赛" fixed placeholder class="w-full" />
+    <van-row class="w-full m-4">
+      <van-col :span="8">
+        <van-popover v-model:show="showEuropePopover">
+          <van-grid square clickable :border="false" column-num="3" style="width: 240px;" >
+            <van-grid-item v-for="i in 6" :key="i" text="选项" icon="photo-o" @click="showEuropePopover = false" />
+          </van-grid>
+          <template #reference>
+            <van-button type="primary">自定义内容</van-button>
+          </template>
+        </van-popover>
+      </van-col>
+    </van-row>
     <van-pull-refresh class="list-container" v-model="isLoading" @refresh="onRefreshList">
       <match-item v-for="match in matchList" :key="match.fid" :match="match" />
     </van-pull-refresh>
@@ -18,6 +30,9 @@ defineOptions({
 })
 const isLoading = ref(false)
 const matchList = ref<IMatchInfo[]>([])
+const showEuropePopover = ref(false)
+const showAsiaPopover = ref(false)
+const showSizePopover = ref(false)
 const onRefreshList = () => {
   isLoading.value = true
   getMatchList().then(res => {
