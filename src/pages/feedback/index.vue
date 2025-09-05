09283612610e4a6ebaa9175b73c4ad67e49a8813
陈昭良 <295183917@qq.com>
2025-09-05 20:55:56 +0800

    修改群

--- src/pages/feedback/index.vue
+++ src/pages/feedback/index.vue
@@ -9,8 +9,7 @@
         以23/24英超第22轮利物浦主场对阵切尔西为例，利物浦赛前排名第一，积分48，切尔西赛前排名第10，积分31，每家公司开出初赔，初赔主队水位，客队水位，以及即时赔率，即时赔率主队水位，客队水位。<br>
         通过大数据找出历史比赛中，初赔同样让球，主客队水位相同的比赛(误差±0.015)，即时赔率同样让球，主客队水位相同的比赛，如果是联赛，且联赛赛程已经超过1/4，因为利物浦排名比切尔西高，再加入筛选条件主队排名高于客队的比赛，以及主队积分>=客队积分+(48-31)的比赛，最终将找到的所有比赛计算赢盘输盘的场次，来得出赢盘输盘的概率。
       </div>
-      <span class="mt-3 text-xl block text-left w-full text-emerald-700">加群讨论</span>
-      <van-image :src="getAssetsFile('wx.jpg')" class="pay"/>
+      <a class="mt-3 text-xl block text-left w-full text-blue-600 underline" href="https://t.me/+M1O5yLtJVGpmNmFl">加飞机群讨论</a>
       <span class="mt-3 text-xl block text-left w-full text-green-800">请作者吃碗沙茶面</span>
       <van-image :src="getAssetsFile('alipay.jpg')" class="pay" />
       <van-image :src="getAssetsFile('wxpay.jpg')" class="pay"/>
